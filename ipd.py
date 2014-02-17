c438c0f9a59936dd77c9ca029be84249bb088b16
exe <exe@zotac>
2014-02-17 14:25:18 -0300

    game simulation refactored

--- ipd.py
+++ ipd.py
@@ -3,68 +3,56 @@ print "IPD";
 import random;
 
 #params
-T=5;
-R=3;
-P=1;
-S=0;
+scores={'T':5,'R':3,'P':1,'S':0};
+N=100;
 
 
 #line that will be modified(input)
-p=[None,0.5,0.5,0.5,0.5];
+p={'cc':0.9,'cd':0.8,'dc':0.05,'dd':0.0};#[p1,p2,p3,p4];
 
 #random strategy for Y player
-random.seed(1);
-q=[None,random.uniform(0,1),random.uniform(0,1),random.uniform(0,1),random.uniform(0,1)];
+random.seed();
+q={'cc':random.uniform(0,1),'cd':random.uniform(0,1),'dc':random.uniform(0,1),'dd':random.uniform(0,1)};#[q1,q2,q3,q4]
 
 #init a fake last iteration result
 xy='cc'
+xScore=0;
+yScore=0;
 
 
-for n in range(1, 10):
+for n in range(1, N):
   #game begins
   xChoice=random.uniform(0,1);
   yChoice=random.uniform(0,1);
-
+  
+  #X chooses if cooperate or defect, based on last game iteration
+  if(0<= xChoice <=p[xy]):
+    choice='c';
+  else:
+    choice='d';
+  
+  #Y chooses if cooperate or defect, based on last game iteration
+  if(0<= yChoice <=q[xy]):
+    choice=choice+'c';
+  else:
+    choice=choice+'d';  
+  
+  xy=choice;
+  ##TODO:CHECK THIS
   if xy=='cc':
-    if(0<= xChoice <=p[1]):
-      xy='c';
-    else:
-      xy='d';
-    
-    if(0<= yChoice <=q[1]):
-      xy=xy+'c';
-    else:
-      xy=xy+'d';
-      
+    xScore+=scores['R'];
+    yScore+=scores['R'];
   elif xy=='cd':
-    if(0<= xChoice <=p[2]):
-      xy='c';
-    else:
-      xy='d';
-    
-    if(0<= yChoice <=q[2]):
-      xy=xy+'c';
-    else:
-      xy=xy+'d';
+    xScore+=scores['S'];
+    yScore+=scores['T'];
   elif xy=='dc':
-    if(0<= xChoice <=p[3]):
-      xy='c';
-    else:
-      xy='d';
-    
-    if(0<= yChoice <=q[3]):
-      xy=xy+'c';
-    else:
-      xy=xy+'d';
+    xScore+=scores['T'];
+    yScore+=scores['S'];
   elif xy=='dd':
-    if(0<= xChoice <=p[4]):
-      xy='c';
-    else:
-      xy='d';
-    
-    if(0<= yChoice <=q[4]):
-      xy=xy+'c';
-    else:
-      xy=xy+'d';
+    xScore+=scores['P'];
+    yScore+=scores['P'];
+  
+  #print xy;
 
-  print xy;
+print xScore/N;
+print yScore/N;
